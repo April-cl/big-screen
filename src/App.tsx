9fc4cec0c90ba1e9e007da729691e79a8a26a148
April <xcl8202@gmail.com>
2021-06-03 00:09:56 +0800

    尝试学习 react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,65 @@
-import React from 'react';
+// import React from 'react';
+//
+//
+// function App() {
+//   return (
+//     <div className="App">
+//     </div>
+//   );
+// }
+//
+// export default App;
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* <Switch>会遍历所有子<Route>，然后渲染第一个成功匹配到当前URL的 */}
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
