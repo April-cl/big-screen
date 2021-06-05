518bd759e6288adc5cf55364793fa82ea9247155
April-cl <xcl8202@gmail.com>
2021-06-05 17:24:48 +0800

    完成 header

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './home.scss'
 
 export default function Home () {
   return (
-    <div>
-      home
+    <div className="home">
+      <header>X X 省基层治理平台 - 决策支持大屏</header>
     </div>
   )
 }
