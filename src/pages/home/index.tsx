91a736a104670b11550674a3252eebf0fb426f50
April-cl <xcl8202@gmail.com>
2021-06-06 01:14:12 +0800

    完成网格布局

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,6 +5,13 @@ export default function Home () {
   return (
     <div className="home">
       <header>X X 省基层治理平台 - 决策支持大屏</header>
+      <main>
+        {/* className中l代表左，m代表中，r代表右，t代表上，b代表下 */}
+        <section className="section1">section1</section>
+        <section className="section2">section1</section>
+        <section className="section3">section1</section>
+        <section className="section4">section1</section>
+      </main>
     </div>
   )
 }
