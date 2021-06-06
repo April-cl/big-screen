3c996400ec29a89e1c1dddae60925bf878000235
April-cl <xcl8202@gmail.com>
2021-06-06 15:40:10 +0800

    完成模块标题样式

---
+++ src/components/real-timeDynamics.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const RealTimeDynamics = () => {
+  return (
+    <div>
+      <h2 className='componentName'><span>实时动态</span></h2>
+    </div>
+  )
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import './home.scss'
+import { RealTimeDynamics } from '../../components/real-timeDynamics';
 
 export default function Home () {
   return (
     <div className="home">
-      <header>X X 省基层治理平台 - 决策支持大屏</header>
+      <header>
+        X X 省基层治理平台 - 决策支持大屏
+      </header>
       <main>
-        {/* className中l代表左，m代表中，r代表右，t代表上，b代表下 */}
-        <section className="section1">section1</section>
-        <section className="section2">section1</section>
-        <section className="section3">section1</section>
-        <section className="section4">section1</section>
+        <section className="section1">
+          <RealTimeDynamics />
+        </section>
+        <section className="section2">section2</section>
+        <section className="section3">section3</section>
+        <section className="section4">section4</section>
       </main>
     </div>
   )
