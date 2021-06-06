a70f504c3fb49505549c8f9c48b6386f66e9057c
April-cl <xcl8202@gmail.com>
2021-06-06 17:21:52 +0800

    完成【今日事件数】组件

--- src/components/real-timeDynamics.tsx
+++ src/components/real-timeDynamics.tsx
@@ -4,6 +4,15 @@ export const RealTimeDynamics = () => {
   return (
     <div>
       <h3 className='componentName'>今日事件数</h3>
+      <div className='eventNumber'>
+        <span className="bordered">0</span>
+        <span className="bordered">8</span>
+        <span className="bordered">2</span>
+        <span className="bordered">3</span>
+        <span className="bordered">7</span>
+        <span className="bordered">9</span>
+        <span>件</span>
+      </div>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -5,16 +5,14 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import './shared/style.scss'
 
 ReactDOM.render(
-  <div style={{background: 'radial-gradient(ellipse closest-side, rgb(11, 39, 69), rgb(8, 22, 49))', height: '100%'}}>
-    <BrowserRouter>
-      <Suspense fallback={<div></div>}>
-        <Switch>
-          <Route path='/' render={routerProps => {
-            return <App {...routerProps}/>
-          }}/>
-        </Switch>
-      </Suspense>
-    </BrowserRouter>
-  </div>,
+  <BrowserRouter>
+    <Suspense fallback={<div></div>}>
+      <Switch>
+        <Route path='/' render={routerProps => {
+          return <App {...routerProps}/>
+        }}/>
+      </Switch>
+    </Suspense>
+  </BrowserRouter>,
   document.getElementById('root')
 );
