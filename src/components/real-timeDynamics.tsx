71b6e61a14b9d2d7fbbff54b61b3c1bf173072e5
April-cl <xcl8202@gmail.com>
2021-06-07 00:58:17 +0800

    完成三个统计圆圈

--- src/components/real-timeDynamics.tsx
+++ src/components/real-timeDynamics.tsx
@@ -18,6 +18,14 @@ export const RealTimeDynamics = () => {
           <span className="eventStatisticsNumber">7132</span>
           <span className="eventStatisticsTitle">今年事件数（万件）</span>
         </div>
+        <div className="circleBorder">
+          <span className="eventStatisticsNumber">89.5</span>
+          <span className="eventStatisticsTitle">今年办结率（%）</span>
+        </div>
+        <div className="circleBorder">
+          <span className="eventStatisticsNumber">1023</span>
+          <span className="eventStatisticsTitle">平均办结时长（h）</span>
+        </div>
       </div>
     </div>
   )
