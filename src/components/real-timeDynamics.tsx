e7303dc30deb8fcd4ceaa049c0508b573a4019bc
April-cl <xcl8202@gmail.com>
2021-06-07 00:17:02 +0800

    完成第一个统计圆圈

--- src/components/real-timeDynamics.tsx
+++ src/components/real-timeDynamics.tsx
@@ -5,13 +5,19 @@ export const RealTimeDynamics = () => {
     <div>
       <h3 className='componentName'>今日事件数</h3>
       <div className='eventNumber'>
-        <span className="bordered">0</span>
-        <span className="bordered">8</span>
-        <span className="bordered">2</span>
-        <span className="bordered">3</span>
-        <span className="bordered">7</span>
-        <span className="bordered">9</span>
+        <span className="rectangleBorder">0</span>
+        <span className="rectangleBorder">8</span>
+        <span className="rectangleBorder">2</span>
+        <span className="rectangleBorder">3</span>
+        <span className="rectangleBorder">7</span>
+        <span className="rectangleBorder">9</span>
         <span>件</span>
+      </div>
+      <div className="eventStatistics">
+        <div className="circleBorder">
+          <span className="eventStatisticsNumber">7132</span>
+          <span className="eventStatisticsTitle">今年事件数（万件）</span>
+        </div>
       </div>
     </div>
   )
