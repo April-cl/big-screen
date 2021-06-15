131f76d9e339422af8601364c169c6796ce29958
April-cl <xcl8202@gmail.com>
2021-06-15 16:32:48 +0800

    重构综合执法组件

--- src/components/lawEnforcement.tsx
+++ src/components/lawEnforcement.tsx
@@ -1,32 +1,35 @@
 import React from 'react';
 
 export const LawEnforcement = () => {
+  const data = [2849, 37291, 9782, 7271];
   return (
-    <div className='lawEnforcement'>
+    <div className="lawEnforcement">
       <div className="circleBorder">
         <div className="lawEnforcementContain">
           <div className="lawEnforcementItem rectangleBorder">
-            <span className="itemValue">2849</span>
+            <span className="itemValue">{data[0]}</span>
             <span className="itemTitle">五水共治</span>
           </div>
           <div className="lawEnforcementItem rectangleBorder">
-            <span className="itemValue">37291</span>
+            <span className="itemValue">{data[1]}</span>
             <span className="itemTitle">安全生产</span>
           </div>
           <div className="lawEnforcementItem rectangleBorder">
-            <span className="itemValue">9782</span>
+            <span className="itemValue">{data[2]}</span>
             <span className="itemTitle">市容环境</span>
           </div>
           <div className="lawEnforcementItem rectangleBorder">
-            <span className="itemValue">7271</span>
+            <span className="itemValue">{data[3]}</span>
             <span className="itemTitle">四边三化</span>
           </div>
         </div>
         <div className="lawEnforcementSum">
           <span>综合执法总数：</span>
-          <span>173840</span>
+          <span>{data.reduce((previous, current) => {
+            return previous + current;
+          })}</span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
