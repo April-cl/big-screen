a00978af5a432e572de15f6a67e9e28a5ddde95a
April-cl <xcl8202@gmail.com>
2021-06-16 16:39:36 +0800

    引入index.js导致显示不正常，所以删掉了

--- public/index.js
+++
@@ -1,9 +0,0 @@
-const {clientWidth, clientHeight} = document.documentElement
-window.pageWidth = (clientWidth / clientHeight) > (16 / 9) ? clientHeight * (16 / 9) : clientWidth
-const pageHeight = pageWidth / (16 / 9)
-const initFontSize = `<style>html{font-size: ${pageWidth / 100}px}</style> `
-
-document.write(initFontSize)
-root.style.width = pageWidth + 'px'
-root.style.height = pageHeight + 'px'
-root.style.marginTop = (clientHeight - pageHeight) / 2 + 'px'
