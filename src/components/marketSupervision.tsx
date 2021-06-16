88acf12d84f73004d3e618dc387cc152473eeb98
April-cl <xcl8202@gmail.com>
2021-06-16 16:03:29 +0800

    模拟动态更新

--- src/components/marketSupervision.tsx
+++ src/components/marketSupervision.tsx
@@ -5,74 +5,74 @@ import { createEchartsOptions } from '../shared/create-echarts-options';
 
 export const MarketSupervision = () => {
   const divRef = useRef(null);
+  const city = ['广州', '韶关', '深圳', '珠海', '汕头', '佛山', '江门', '湛江', '茂名', '肇庆', '惠州', '梅州', '汕尾', '河源', '阳江', '清远', '东莞', '中山', '潮州', '揭阳', '云浮'];
+  let data = [];
+  for (let i = 0; i <= city.length; i++) {
+    data[i] = [];
+    data[i][0] = city[i];
+    data[i][1] = Math.floor(Math.random() * 100);
+    data[i][2] = Math.floor(Math.random() * 100);
+    data[i][3] = Math.floor(Math.random() * 100);
+  }
+  data.unshift(['市名', '药械安全', '食品安全', '证照管理']);
   useEffect(() => {
     const myChart = echarts.init(divRef.current);
-    myChart.setOption(createEchartsOptions({
-      legend: {
-        textStyle: {
-          color: 'rgb(254, 251, 252)'
+    const updateData = (data) => {
+      myChart.setOption(createEchartsOptions({
+        legend: {
+          textStyle: {
+            color: 'rgb(254, 251, 252)'
+          },
+          right: 0,
         },
-        right: 0,
-      },
-      tooltip: {},
-      dataset: {
-        source: [
-          ['省名', '药械安全', '食品安全', '证照管理'],
-          ['广州', 43.3, 85.8, 93.7],
-          ['韶关', 83.1, 73.4, 55.1],
-          ['深圳', 86.4, 65.2, 82.5],
-          ['珠海', 72.4, 53.9, 39.1],
-          ['汕头', 43.3, 85.8, 93.7],
-          ['佛山', 83.1, 73.4, 55.1],
-          ['江门', 86.4, 65.2, 82.5],
-          ['湛江', 43.3, 85.8, 93.7],
-          ['茂名', 83.1, 73.4, 55.1],
-          ['肇庆', 86.4, 65.2, 82.5],
-          ['惠州', 43.3, 85.8, 93.7],
-          ['梅州', 83.1, 73.4, 55.1],
-          ['汕尾', 86.4, 65.2, 82.5],
-          ['河源', 43.3, 85.8, 93.7],
-          ['阳江', 83.1, 73.4, 55.1],
-          ['清远', 86.4, 65.2, 82.5],
-          ['东莞', 43.3, 85.8, 93.7],
-          ['中山', 83.1, 73.4, 55.1],
-          ['潮州', 86.4, 65.2, 82.5],
-          ['揭阳', 43.3, 85.8, 93.7],
-          ['云浮', 83.1, 73.4, 55.1]
+        tooltip: {},
+        dataset: {
+          source: data
+        },
+        xAxis: {
+          type: 'category',
+          axisLabel: {
+            interval: 0,
+            formatter: function (value) {
+              return value.split('').join('\n');
+            }
+          }
+        },
+        yAxis: {
+          splitLine: {
+            lineStyle: {
+              color: ['rgba(32, 48, 73, 1)']
+            }
+          }
+        },
+        grid: {
+          x: px(36),
+          y: px(30),
+          x2: 0,
+          y2: px(42)
+        },
+        series: [
+          {type: 'bar'},
+          {type: 'bar'},
+          {type: 'bar'}
+        ],
+        dataZoom: [
+          {
+            type: 'inside'
+          }
         ]
-      },
-      xAxis: {
-        type: 'category',
-        axisLabel: {
-          interval: 0,
-          formatter: function (value) {
-            return value.split('').join('\n');
-          }
-        }
-      },
-      yAxis: {
-        splitLine: {
-          lineStyle: {
-            color: ['rgba(32, 48, 73, 1)']
-          }
-        }},
-      grid: {
-        x: px(36),
-        y: px(30),
-        x2: 0,
-        y2: px(42)
-      },
-      series: [
-        {type: 'bar'},
-        {type: 'bar'},
-        {type: 'bar'}
-      ],
-      dataZoom: [
-        {
-          type: 'inside'
-        }
-      ]
-    }));
+      }));
+    };
+    setInterval(() => {
+      for (let i = 0; i <= city.length; i++) {
+        data[i+1][1] = Math.floor(Math.random() * 100);
+        data[i+1][2] = Math.floor(Math.random() * 100);
+        data[i+1][3] = Math.floor(Math.random() * 100);
+      }
+      updateData(data);
+    }, 1000);
+    updateData(data);
+
   }, []);
   return (
     <div ref={divRef} className="marketSupervision"></div>
