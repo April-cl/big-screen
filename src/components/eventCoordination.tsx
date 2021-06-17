84cbf662d472b06bb783f40b767f67d8317f03a2
April-cl <xcl8202@gmail.com>
2021-06-17 17:40:05 +0800

    事件协同表格滚动显示

--- src/components/eventCoordination.tsx
+++ src/components/eventCoordination.tsx
@@ -24,6 +24,26 @@ export const EventCoordination = () => {
       name: '某某市民情双访数据平台',
       numbers: 8732
     },
+    {
+      name: '平安建设信息系统',
+      numbers: 7832
+    },
+    {
+      name: '统一政务咨询投诉举报平台',
+      numbers: 58829
+    },
+    {
+      name: '某某市智慧城管系统',
+      numbers: 18319
+    },
+    {
+      name: '某某区智慧河道云平台',
+      numbers: 38123
+    },
+    {
+      name: '某某市民情双访数据平台',
+      numbers: 8732
+    },
   ]
   useEffect(() => {
     for (let i = 0; i < data.length; i++) {
@@ -37,11 +57,9 @@ export const EventCoordination = () => {
     <div className="eventCoordination">
       <table className="eventCoordination">
         <thead>
-        <tr>
           <th>序号</th>
           <th>系统名称</th>
           <th>协同事件数</th>
-        </tr>
         </thead>
         <tbody ref={divRef}>
         </tbody>
